42df120551cae72a34a875215b9c6460ecc06b8d
よしひさ <t.goto0482@live.jp>
2018-10-03 15:38:36 +0900

    NT金沢

---
+++ firmware/EDAMAME_S/EDAMAME_S.ino
@@ -0,0 +1,180 @@
+#include <Wire.h>
+#define LSM303AGR_A 0x19
+#define LSM303AGR_M 0x1E
+#define INA226    0x45
+
+#define MOTOR_LF  25
+#define MOTOR_LR  26
+#define MOTOR_RF  14
+#define MOTOR_RR  27
+#define NICHROME  13
+#define SD_CS     4
+
+HardwareSerial Serial2(2);
+TaskHandle_t th_gps;
+
+struct Compass{
+  int16_t x;
+  int16_t y;
+  int16_t z;
+  int16_t x_offset=0;
+  int16_t y_offset=0;
+  int16_t z_offset=0;
+  double arg;
+};
+
+struct Power{
+  int32_t v;
+  int32_t a;
+  double voltage;
+  double current;
+};
+
+struct gps {
+  double latitude;
+  double longitude;
+  int8_t hh, mm, ss;
+  long state;
+  int16_t quality;
+
+};
+
+struct gps gps_data;
+
+void setup() {
+
+  struct Compass compass;
+  struct Power power;
+
+  //シリアルポート初期化
+  Serial.begin(115200);
+  Serial2.begin(9600);
+
+  //I2C初期化
+  Wire.begin(21, 22);
+  byte val;
+  //地磁気設定
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x60);
+  Wire.write(0b00001101);  
+  Wire.endTransmission(true);
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x61);
+  Wire.write(0b00000100);  
+  Wire.endTransmission(true);
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x62);
+  Wire.write(0b00010000);  
+  Wire.endTransmission(true);
+  //電流電圧計設定
+  Wire.beginTransmission(INA226);
+  Wire.write(0x00);
+  Wire.write(0b01000101);  
+  Wire.write(0b00100111);    
+  Wire.endTransmission(true);
+
+  //PWM初期化
+  ledcSetup(0, 1000, 10);
+  ledcAttachPin(MOTOR_LF, 0);
+  ledcAttachPin(MOTOR_LR, 1);
+  ledcAttachPin(MOTOR_RF, 2);
+  ledcAttachPin(MOTOR_RR, 3);
+  
+  ledcWrite(0, 0);
+  ledcWrite(1, 0);
+  ledcWrite(2, 0);
+  ledcWrite(3, 0);
+
+  //ピン初期化
+  pinMode(NICHROME, OUTPUT);
+  digitalWrite(NICHROME, LOW);
+
+  xTaskCreatePinnedToCore(get_gps, "GET_GPS", 4096, NULL, 2, &th_gps, 0);
+
+  //地磁気キャリブレーション
+  compass_calibration(&compass, 1000);
+  uint32_t cnt=0;
+  while(1){
+    get_compass(&compass);
+    get_powerState(&power);
+
+    if(cnt%10==0){    
+      Serial.printf("X:%4d Y:%4d Z:%4d arg:%5.2f %8.4f[A] %8.4f[V] ",compass.x,compass.y,compass.z,compass.arg ,power.current ,power.voltage );
+      Serial.printf("GPS_mode=%d  %02d:%02d:%02d %12.7f %12.7f",gps_data.quality ,gps_data.hh, gps_data.mm ,gps_data.ss, gps_data.latitude ,gps_data.longitude );
+      Serial.println("");
+    }
+
+    if(compass.arg < -0.3){
+      motor(200,0);
+    }
+
+    if(compass.arg > 0.3){
+      motor(0,200);
+    }
+    delay(10);
+
+    cnt++;
+  }
+}
+
+void loop()
+{
+
+}
+
+void nichrome(int power){
+    ledcWrite(4, power);
+}
+
+//地磁気センサのキャリブレーション
+void compass_calibration(struct Compass * Compass , long cycle) {
+  int16_t x, y,  z, x_max, x_min, y_max, y_min;
+  motor(200, 600);
+
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x60);
+  Wire.write(0b00001101);  
+  Wire.endTransmission(true);
+
+  //最小値・最大値を最初の測定値で初期化
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x68);
+  Wire.endTransmission(false);
+  Wire.requestFrom(LSM303AGR_M, 6);
+  x = Wire.read();
+  x += (Wire.read()<<8);
+  y = Wire.read();
+  y += (Wire.read()<<8);
+  z = Wire.read();
+  z += (Wire.read()<<8);
+
+  x_max = x ; x_min = x ;
+  y_max = y ; y_min = y ;
+
+  for (long i = 0; i < cycle; i++) {
+    Wire.beginTransmission(LSM303AGR_M);
+    Wire.write(0x60);
+    Wire.write(0b00001101);  
+    Wire.endTransmission(true);
+    
+    Wire.beginTransmission(LSM303AGR_M);
+    Wire.write(0x68);
+    Wire.endTransmission(false);
+    Wire.requestFrom(LSM303AGR_M, 6);
+    x = Wire.read();
+    x += (Wire.read()<<8);
+    y = Wire.read();
+    y += (Wire.read()<<8);
+    z = Wire.read();
+    z += (Wire.read()<<8);
+    if (x > x_max)x_max = x;
+    if (x < x_min)x_min = x;
+    if (y > y_max)y_max = y;
+    if (y < y_min)y_min = y;
+    delay(10);
+  }
+  Compass -> x_offset = (x_max + x_min) / 2;
+  Compass -> y_offset = (y_max + y_min) / 2;
+  motor( 0, 0);
+  return;
+}
---
+++ firmware/EDAMAME_S/edamame_func.ino
@@ -0,0 +1,259 @@
+
+//モーターの速度変更
+void motor(int l ,int r){
+  if(l>=0){
+    ledcWrite(0, l);
+    ledcWrite(1, 0);
+  }else{
+    ledcWrite(0, 0);    
+    ledcWrite(1, -1*l);
+  }
+
+  if(r>=0){
+    ledcWrite(2, r);
+    ledcWrite(3, 0);
+    
+  }else{
+    ledcWrite(2, 0);    
+    ledcWrite(3, -1*r);
+  }
+}
+
+//電圧を読む
+void get_powerState(Power *data){
+  //電流
+  Wire.beginTransmission(INA226);
+  Wire.write(0x01);
+  Wire.endTransmission(false);
+  Wire.requestFrom(INA226, 2);
+  data->a = 0;
+  data->a |= (Wire.read()<<8);
+  data->a |= Wire.read();
+
+  //電圧
+  Wire.beginTransmission(INA226);
+  Wire.write(0x02);
+  Wire.endTransmission(false);
+  Wire.requestFrom(INA226, 2);
+  data->v = 0;
+  data->v |= (Wire.read()<<8);
+  data->v |= Wire.read();
+
+  data->current = 0.00025 * data->a;
+  data->voltage = 0.00125 * data->v;
+}
+
+//地磁気の値を詠む
+void get_compass(Compass *data){
+  int16_t x, y, z;
+  
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x60);
+  Wire.write(0b00001101);  
+  Wire.endTransmission(true);
+  
+  Wire.beginTransmission(LSM303AGR_M);
+  Wire.write(0x68);
+  Wire.endTransmission(false);
+  Wire.requestFrom(LSM303AGR_M, 6);
+  x = Wire.read();
+  x += (Wire.read()<<8);
+  y = Wire.read();
+  y += (Wire.read()<<8);
+  z = Wire.read();
+  z += (Wire.read()<<8);
+
+  data -> x = x - data->x_offset;
+  data -> y = y - data->y_offset;
+  data -> z = z - data->z_offset;
+  data -> arg = -1.0 * atan2((double) - 1 * data -> y, (double)data -> x);
+
+  return;
+}
+
+//GPS座標取得
+void get_gps(void *pvParameters) {
+  char c, gps_msg[96];
+  long GPS_stat = 0;
+  long dd , mm, mmmm, ddd, dddd;
+  while(1) {
+
+    //GPSのセンテンスの始まりを探してデータタイプの種類を代入
+    c = get_gps_1();
+    if (c == '$') {
+      for (long i = 0 ; i < 6 ; i++) {
+        c = get_gps_1();
+        gps_msg[i] = c;
+      }
+      //時刻・座標の取得計算
+      if (strncmp(gps_msg, "GPGGA", 5) == 0) {
+        for (long i = 0 ; c != '\n' ; i++) {
+          c = get_gps_1();
+          gps_msg[i] = c;
+
+          //1行取得し終えたら
+          if (c == '\n') {
+            gps_data.latitude = 0;
+            gps_data.longitude = 0;
+            gps_data.hh = 0;
+            gps_data.mm = 0;
+            gps_data.ss = 0;
+            gps_data.quality = -1;
+            GPS_stat = 0;
+
+            gps_data.hh = 10 * (gps_msg[0] - 48) + (gps_msg[1] - 48);
+            gps_data.mm = 10 * (gps_msg[2] - 48) + (gps_msg[3] - 48);
+            gps_data.ss = 10 * (gps_msg[4] - 48) + (gps_msg[5] - 48);
+            if (gps_msg[6] != '.')GPS_stat |= 0b0000001;
+            if (gps_data.hh < 0 || gps_data.hh > 24)GPS_stat = 0b0000001;
+
+            //測位できていたら
+            if (i > 40) {
+              long n;
+              dd = 0;
+              for (n = 6; n < i; n++) {
+                if (gps_msg[n] == ',') {
+                  dd  = 10 * (gps_msg[n + 1] - 48) + (gps_msg[n + 2] - 48);
+                  mm  = 10 * (gps_msg[n + 3] - 48) + (gps_msg[n + 4] - 48);
+                  mmmm = 1000 * (gps_msg[n + 6] - 48) + 100 * (gps_msg[n + 7] - 48) + 10 * (gps_msg[n + 8] - 48) + (gps_msg[n + 9] - 48);
+
+                  gps_data.latitude = (dd + (((double)mm + (double)mmmm / 10000) / 60 )) ;
+                  if (gps_msg[n + 10] != ',' || gps_msg[n + 11] != 'N')GPS_stat |= 0b0000010;
+                  break;
+                }
+              }
+              for (n = n + 1 ; n < i; n++) {
+                if (gps_msg[n] == 'N') {
+                  break;
+                }
+              }
+              for (n = n + 1 ; n < i; n++) {
+                if (gps_msg[n] == ',') {
+                  ddd  = 100 * (gps_msg[n + 1] - 48) + 10 * (gps_msg[n + 2] - 48) + (gps_msg[n + 3] - 48);
+                  mm  = 10 * (gps_msg[n + 4] - 48) + (gps_msg[n + 5] - 48);
+                  mmmm = 1000 * (gps_msg[n + 7] - 48) + 100 * (gps_msg[n + 8] - 48) + 10 * (gps_msg[n + 9] - 48) + (gps_msg[n + 10] - 48);
+                  gps_data.longitude = (ddd + (((double)mm + (double)mmmm / 10000) / 60 )) ;
+                  if (gps_msg[n + 11] != ',' || gps_msg[n + 12] != 'E')GPS_stat |= 0b0000100;
+                  break;
+                }
+              }
+              for (n = n + 1 ; n < i; n++) {
+                if (gps_msg[n] == 'E') {
+                  break;
+                }
+              }
+              gps_data.quality = (gps_msg[n + 2] - 48);
+            }
+            //測位できていなかったら
+            else   GPS_stat |= 0b0010000;
+            gps_data.state= GPS_stat ;
+            vTaskDelay(800);
+          }
+        }
+      }
+    }
+  }
+}
+
+//GPSから1文字受信
+char get_gps_1() {
+  while (1) {
+    long data = Serial2.read();
+    if (data != -1) {
+      char c = (char)data;
+      return data;
+    }
+  }
+}
+
+/*
+//UARTで制御履歴を送る
+void uart_send() {
+  Serial.print(log_num);
+  Serial.print(",");
+  Serial.print(mode);
+  Serial.print(",");
+  Serial.print(gps.hh);
+  Serial.print(",");
+  Serial.print(gps.mm);
+  Serial.print(",");
+  Serial.print(gps.ss);
+  Serial.print(",");
+  Serial.print(gps.latitude);
+  Serial.print(",");
+  Serial.print(gps.longitude);
+  Serial.print(",");
+  Serial.print(gps_stat);
+  Serial.print(",");
+  Serial.print(Vattery_Voltage);
+  Serial.print(",");
+  Serial.print(HMC5883L.x);
+  Serial.print(",");
+  Serial.print(HMC5883L.y);
+  Serial.print(",");
+  Serial.print(HMC5883L.arg);
+  Serial.print(",");
+  Serial.print(dist);
+  Serial.print(",");
+  Serial.print(a);
+  Serial.print(",");
+  Serial.print(arg);
+  Serial.print(",");
+  Serial.print(motorL);
+  Serial.print(",");
+  Serial.print(motorR);
+  Serial.print(",");
+  Serial.print(US_Distance);
+  Serial.println("");
+  return;
+}
+
+//制御履歴を書き込む
+void SD_write() {
+  String SDString = "";
+  dataFile = SD.open(path, FILE_WRITE);
+  SDString.concat(log_num);
+  SDString.concat(",");
+  SDString.concat(mode);
+  SDString.concat(",");
+  SDString.concat(gps.hh);
+  SDString.concat(",");
+  SDString.concat(gps.mm);
+  SDString.concat(",");
+  SDString.concat(gps.ss);
+  SDString.concat(",");
+  SDString.concat(gps.latitude);
+  SDString.concat(",");
+  SDString.concat(gps.longitude);
+  SDString.concat(",");
+  SDString.concat(gps_stat);
+  SDString.concat(",");
+  SDString.concat(Vattery_Voltage);
+  SDString.concat(",");
+  SDString.concat(HMC5883L.x);
+  SDString.concat(",");
+  SDString.concat(HMC5883L.y);
+  SDString.concat(",");
+  SDString.concat(HMC5883L.arg);
+  SDString.concat(",");
+  SDString.concat(dist);
+  SDString.concat(",");
+  SDString.concat(a);
+  SDString.concat(",");
+  SDString.concat(arg);
+  SDString.concat(",");
+  SDString.concat(motorL);
+  SDString.concat(",");
+  SDString.concat(motorR);
+  SDString.concat(",");
+  SDString.concat(US_Distance);
+  SDString.concat("\n");
+  dataFile.print(SDString);
+  dataFile.close();
+  if (log_num % 2 == 0)digitalWrite(LED1, HIGH);
+  digitalWrite(LED1, LOW);
+  log_num++;
+  return;
+}
+*/
+
