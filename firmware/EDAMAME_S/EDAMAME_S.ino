86fdd8322465eaef5da40486f2efea9a0984e44d
よしひさ <t.goto0482@live.jp>
2018-10-23 22:17:05 +0900

    edamame_func.inoからほとんどの関数をクラスライブラリに移行

--- firmware/EDAMAME_S/EDAMAME_S.ino
+++ firmware/EDAMAME_S/EDAMAME_S.ino
@@ -8,36 +8,12 @@
     https://dl.espressif.com/dl/package_esp32_dev_index.json
 */
 #include <Wire.h>
-#include"esp32-hal-ledc.h"
-#define LSM303AGR_A 0x19
-#define LSM303AGR_M 0x1E
-#define INA226      0x45
+#include "edamame_core.h"
+#include "edamame_ex.h"
 
-#define MOTOR_LF  25
-#define MOTOR_LR  26
-#define MOTOR_RF  14
-#define MOTOR_RR  27
-#define NICHROME  13
-#define SD_CS     4
 
 TaskHandle_t th_gps;
 
-struct Compass{
-  int16_t x;
-  int16_t y;
-  int16_t z;
-  int16_t x_offset=0;
-  int16_t y_offset=0;
-  int16_t z_offset=0;
-  double arg;
-};
-
-struct Power{
-  int32_t v;
-  int32_t a;
-  double voltage;
-  double current;
-};
 
 struct gps {
   double latitude;
@@ -52,74 +28,45 @@ struct gps gps_data;
 
 void setup() {
 
-  struct Compass compass;
-  struct Power power;
-
   //シリアルポート初期化
   Serial.begin(115200);
   Serial2.begin(9600);
 
   //I2C初期化
   Wire.begin(21, 22);
+
   //地磁気設定
-  Wire.beginTransmission(LSM303AGR_M);
-  Wire.write(0x60);
-  Wire.write(0b00001101);  
-  Wire.endTransmission(true);
-  Wire.beginTransmission(LSM303AGR_M);
-  Wire.write(0x61);
-  Wire.write(0b00000100);  
-  Wire.endTransmission(true);
-  Wire.beginTransmission(LSM303AGR_M);
-  Wire.write(0x62);
-  Wire.write(0b00010000);  
-  Wire.endTransmission(true);
+  LSM303 compass(LSM303AGR_A,LSM303AGR_M);
+  compass.init_m();
+
   //電流電圧計設定
-  
-  Wire.beginTransmission(INA226);
-  Wire.write(0x00);
-  Wire.write(0b01000101);  
-  Wire.write(0b00100111);    
-  Wire.endTransmission(true);
+  Power ina226(INA226);
+  ina226.init();
 
   //PWM初期化
-  pinMode(MOTOR_LF, OUTPUT);
-  pinMode(MOTOR_LR, OUTPUT);
-  pinMode(MOTOR_RF, OUTPUT);
-  pinMode(MOTOR_RR, OUTPUT);
-  
-  ledcSetup(0, 800, 10);
-  ledcSetup(1, 800, 10);
-  ledcSetup(2, 800, 10);
-  ledcSetup(3, 800, 10);
-  ledcAttachPin(MOTOR_LF, 0);
-  ledcAttachPin(MOTOR_LR, 1);
-  ledcAttachPin(MOTOR_RF, 2);
-  ledcAttachPin(MOTOR_RR, 3);
-  ledcWrite(0, 0);
-  ledcWrite(1, 0);
-  ledcWrite(2, 0);
-  ledcWrite(3, 0);
+  Motor motor(0,1,2,3);
+  motor.init(4000,10);
+
+  Nichrome nichrome(4);
 
   //ピン初期化
-  pinMode(NICHROME, OUTPUT);
-  digitalWrite(NICHROME, LOW);
 
-  xTaskCreatePinnedToCore(get_gps, "GET_GPS", 4096, NULL, 2, &th_gps, 0);
+  //xTaskCreatePinnedToCore(get_gps, "GET_GPS", 4096, NULL, 2, &th_gps, 0);
 
   //地磁気キャリブレーション
-  compass_calibration(&compass, 1000);
+  //compass_calibration(&compass, 1000);
   uint32_t cnt=0;
   while(1){
-    get_compass(&compass);
-    get_powerState(&power);
-    motor(700,700);
-    if(cnt%1==0){    
-      Serial.printf("X:%4d Y:%4d Z:%4d arg:%5.2f %8.4f[A] %8.4f[V] ",compass.x,compass.y,compass.z,compass.arg ,power.current ,power.voltage );
+    //motor.speed(cnt%2048-1024,cnt%2048-1024);
+    //nichrome.fire(cnt%255);
+    compass.update_m();
+    ina226.update();
+    if(cnt%10==0){    
+      Serial.printf("X:%4d Y:%4d Z:%4d arg:%5.2f %8.4f[A] %8.4f[V] ",compass.x,compass.y,compass.z,compass.arg ,ina226.current ,ina226.voltage );
       Serial.printf("GPS_mode=%d  %02d:%02d:%02d %12.7f %12.7f",gps_data.quality ,gps_data.hh, gps_data.mm ,gps_data.ss, gps_data.latitude ,gps_data.longitude );
       Serial.println("");
     }
-
+/*
     if(compass.arg < -0.3){
       motor(200,0);
     }
@@ -131,8 +78,8 @@ void setup() {
     if((compass.arg < 0.3)&&(compass.arg > -0.3)){
        motor(0,0);
     }
-    
-    delay(100);
+    */
+    delay(10);
 
     cnt++;
   }
@@ -143,10 +90,7 @@ void loop()
 
 }
 
-void nichrome(int power){
-    ledcWrite(4, power);
-}
-
+/*
 //地磁気センサのキャリブレーション
 void compass_calibration(struct Compass * Compass , long cycle) {
   int16_t x, y,  z, x_max, x_min, y_max, y_min;
@@ -198,4 +142,4 @@ void compass_calibration(struct Compass * Compass , long cycle) {
   Compass -> y_offset = (y_max + y_min) / 2;
   motor( 0, 0);
   return;
-}
+}*/
--- firmware/EDAMAME_S/edamame_core.cpp
+++ firmware/EDAMAME_S/edamame_core.cpp
@@ -2,3 +2,56 @@
 //将来的にedamame_func.inoはこのファイルに移行します。
 
 #include "edamame_core.h"
+
+
+LSM303::LSM303(uint8_t data1,uint8_t data2){
+    address_a =data1;
+    address_m =data2;
+    
+}
+
+void LSM303::init_m(){
+    Wire.beginTransmission(address_m);
+    Wire.write(0x60);
+    Wire.write(0b00001101);  
+    Wire.endTransmission(true);
+    Wire.beginTransmission(address_m);
+    Wire.write(0x61);
+    Wire.write(0b00000100);  
+    Wire.endTransmission(true);
+    Wire.beginTransmission(address_m);
+    Wire.write(0x62);
+    Wire.write(0b00010000);  
+    Wire.endTransmission(true);
+}
+
+void LSM303::update_m(){
+
+    Wire.beginTransmission(address_m);
+    Wire.write(0x60);
+    Wire.write(0b00001101);  
+    Wire.endTransmission(true);
+
+    Wire.beginTransmission(address_m);
+    Wire.write(0x68);
+    Wire.endTransmission(false);
+    Wire.requestFrom(address_m, 6);
+    x = Wire.read();
+    x += (Wire.read()<<8);
+    y = Wire.read();
+    y += (Wire.read()<<8);
+    z = Wire.read();
+    z += (Wire.read()<<8);
+
+    x = x - x_offset;
+    y = y - y_offset;
+    z = z - z_offset;
+    arg = -1.0 * atan2((double) - 1 * y, (double)x);
+
+}
+
+void LSM303::init_a(){
+}
+
+void LSM303::update_a(){
+}
